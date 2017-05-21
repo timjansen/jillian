0bd0ba6fca351c2ae87b7153a35cf25dbb2d7f69
tim <tim@tjansen.de>
2017-05-21 17:54:48 -0400

    avoiding lamda in mocha

--- test/jel/parser-test.js
+++ test/jel/parser-test.js
@@ -3,10 +3,10 @@
 const assert = require('assert');
 const JEL = require('../../src/jel/jel.js');
 
-describe('jelParser', ()=>{
-  describe('parseExpression()', ()=>{
+describe('jelParser', function() {
+  describe('parseExpression()', function() {
     
-    it('should parse a simple literal', ()=>{
+    it('should parse a simple literal', function() {
       assert.deepEqual(new JEL('5').parseTree, {type: 'literal', value: 5});
       assert.deepEqual(new JEL('+5').parseTree, {type: 'literal', value: 5});
       assert.deepEqual(new JEL('-5').parseTree, {type: 'literal', value: -5});
@@ -14,33 +14,33 @@ describe('jelParser', ()=>{
       assert.deepEqual(new JEL('"foo"').parseTree, {type: 'literal', value: 'foo'});
     });
     
-    it('should parse variables', ()=>{
+    it('should parse variables', function() {
       assert.deepEqual(new JEL('a').parseTree, {type: 'variable', name: 'a'});
       assert.deepEqual(new JEL('a_b_c').parseTree, {type: 'variable', name: 'a_b_c'});
     });
 
-    it('should parse some unary operations', ()=>{
+    it('should parse some unary operations', function() {
       assert.deepEqual(new JEL('!a').parseTree, {type: 'operator', operator: '!', operand: {type: 'variable', name: 'a'}});
       assert.deepEqual(new JEL('abs -5').parseTree, {type: 'operator', operator: 'abs', operand: {type: 'literal', value: -5}});
     });
     
-    it('should parse some binary operations', ()=>{
+    it('should parse some binary operations', function() {
       assert.deepEqual(new JEL('a+b').parseTree, {type: 'operator', operator: '+', left: {type: 'variable', name: 'a'}, right: {type: 'variable', name: 'b'}});
       assert.deepEqual(new JEL('a+b+c').parseTree, {type: 'operator', operator: '+', left: {type: 'operator', operator: '+', left: {type: 'variable', name: 'a'}, right: {type: 'variable', name: 'b'}}, right: {type: 'variable', name: 'c'}});
     });
 
-    it('should support precedence', ()=>{
+    it('should support precedence', function() {
       assert.deepEqual(new JEL('a+b*c').parseTree, {type: 'operator', operator: '+', left: {type: 'variable', name: 'a'}, right: {type: 'operator', operator: '*', left: {type: 'variable', name: 'b'}, right: {type: 'variable', name: 'c'}}});
     });
  
-    it('should support parens', ()=>{
+    it('should support parens', function() {
       assert.deepEqual(new JEL('(b+c)').parseTree, {type: 'operator', operator: '+', left: {type: 'variable', name: 'b'}, right: {type: 'variable', name: 'c'}});
       assert.deepEqual(new JEL('(((((b+c)))))').parseTree, {type: 'operator', operator: '+', left: {type: 'variable', name: 'b'}, right: {type: 'variable', name: 'c'}});
       assert.deepEqual(new JEL('a+(b+c)').parseTree, {type: 'operator', operator: '+', left: {type: 'variable', name: 'a'}, right: {type: 'operator', operator: '+', left: {type: 'variable', name: 'b'}, right: {type: 'variable', name: 'c'}}});
       assert.deepEqual(new JEL('(a+b)+c+(((d*e)))').parseTree, new JEL('a+b+c+d*e').parseTree);
     });
 
-    it('should support function calls', ()=>{
+    it('should support function calls', function() {
       assert.deepEqual(new JEL('f()').parseTree, {type: 'call', argList: [], left: {type: 'variable', name: 'f'}});
       assert.deepEqual(new JEL('f(2)').parseTree, {type: 'call', argList: [{type: 'literal', value: 2}], left: {type: 'variable', name: 'f'}});
       assert.deepEqual(new JEL('f("foo", 2)').parseTree, {type: 'call', argList: [{type: 'literal', value: 'foo'}, {type: 'literal', value: 2}], left: {type: 'variable', name: 'f'}});
@@ -53,7 +53,7 @@ describe('jelParser', ()=>{
       assert.deepEqual(new JEL('g((a,b,c)=>2)').parseTree, {type: 'call', argList: [{type: 'lambda', args: ['a', 'b', 'c'], expression: {type: 'literal', value: 2}}], left: {type: 'variable', name: 'g'}});
     });
   
-    it('should support lambdas', ()=>{
+    it('should support lambdas', function() {
       assert.deepEqual(new JEL('x=>1').parseTree, {type: 'lambda', args: ['x'], expression: {type: 'literal', value: 1}});
       assert.deepEqual(new JEL('()=>1').parseTree, {type: 'lambda', args: [], expression: {type: 'literal', value: 1}});
       assert.deepEqual(new JEL('(a, b)=>a+b').parseTree, {type: 'lambda', args: ['a', 'b'], expression: {type: 'operator', operator: '+', left: {type: 'variable', name: 'a'}, right: {type: 'variable', name: 'b'}}});
@@ -62,17 +62,17 @@ describe('jelParser', ()=>{
       assert.deepEqual(new JEL('((a, b)=>a+b)(1,2)').parseTree, {type: 'call', argList: [{type: 'literal', value: 1},  {type: 'literal', value: 2}], left: {type: 'lambda', args: ['a', 'b'], expression: {type: 'operator', operator: '+', left: {type: 'variable', name: 'a'}, right: {type: 'variable', name: 'b'}}}});
     });
 
-    it('should support if/else', ()=>{
+    it('should support if/else', function() {
       assert.deepEqual(new JEL('if a else b').parseTree, {type: 'condition', condition: {type: 'variable', name: 'a'}, else: {type: 'variable', name: 'b'}});
       assert.deepEqual(new JEL('if (a!=0) else b').parseTree, {type: 'condition', condition: {type: 'operator', operator: '!=', left: {type: 'variable', name: 'a'}, right:{type: 'literal', value: 0}}, else: {type: 'variable', name: 'b'}});
     });
 
-    it('should support if/then/else', ()=>{
+    it('should support if/then/else', function() {
       assert.deepEqual(new JEL('if a then b else c').parseTree, {type: 'condition', condition: {type: 'variable', name: 'a'}, then: {type: 'variable', name: 'b'}, else: {type: 'variable', name: 'c'}});
       assert.deepEqual(new JEL('if (a!=0) then ((b)) else f()').parseTree, {type: 'condition', condition: {type: 'operator', operator: '!=', left: {type: 'variable', name: 'a'}, right: {type: 'literal', value: 0}}, then: {type: 'variable', name: 'b'}, else: {type: 'call', argList: [], left: {type: 'variable', name: 'f'}}});
     });  
     
-    it('should support with', ()=>{
+    it('should support with', function() {
       assert.deepEqual(new JEL('with a: 1 => a').parseTree, {type: 'with', assignments: [{name: 'a', expression: {type: 'literal', value: 1}}], expression: {type: 'variable', name: 'a'}});
       assert.deepEqual(new JEL('with a:1,b:2=>b').parseTree, {type: 'with', assignments: [{name: 'a', expression: {type: 'literal', value: 1}}, {name: 'b', expression: {type: 'literal', value: 2}}], expression: {type: 'variable', name: 'b'}});
       assert.deepEqual(new JEL('with a:1, b: a + 2 => b').parseTree, {type: 'with', assignments: [{name: 'a', expression: {type: 'literal', value: 1}}, {name: 'b', expression: {type: 'operator', operator: '+', left: {type: 'variable', name: 'a'}, right: {type: 'literal', value: 2}}}], expression: {type: 'variable', name: 'b'}});
--- test/jel/tokenizer-test.js
+++ test/jel/tokenizer-test.js
@@ -3,30 +3,30 @@
 const assert = require('assert');
 const jt = require('../../src/jel/tokenizer.js');
 
-describe('jelTokenizer', ()=>{
-  describe('tokenize()', ()=>{
+describe('jelTokenizer', function() {
+  describe('tokenize()', function() {
     
-    it('should parse an empty string', ()=>{
+    it('should parse an empty string', function() {
       assert.deepEqual(jt.tokenize('').tokens, []);
       assert.deepEqual(jt.tokenize('   ').tokens, []);
     });
 
-     it('should parse operators', ()=>{
+     it('should parse operators', function() {
       assert.deepEqual(jt.tokenize('<=+').tokens, [{value: '<=', operator: true}, {value: '+', operator: true}]);
       assert.deepEqual(jt.tokenize('!a').tokens, [{value: '!', operator: true}, {value: 'a', identifier: true}]);
       assert.deepEqual(jt.tokenize('(a)').tokens, [{value: '(', operator: true}, {value: 'a', identifier: true}, {value: ')', operator: true}]);
     });
 
-    it('should parse identifiers', ()=>{
+    it('should parse identifiers', function() {
       assert.deepEqual(jt.tokenize('a $_b0 c_d_093_d').tokens, [{value: 'a', identifier: true}, {value: '$_b0', identifier: true}, {value: 'c_d_093_d', identifier: true}]);
     });
 
-    it('should parse literals', ()=>{
+    it('should parse literals', function() {
       assert.deepEqual(jt.tokenize('3.5 null true "hello" `hi`').tokens, [{value: 3.5, type: 'literal'}, {value: null, type: 'literal'}, {value: true, type: 'literal'},
                                                                          {value: 'hello', type: 'literal'}, {value: 'hi', type: 'literal'}]);
     });
 
-    it('should parse expressions', ()=>{
+    it('should parse expressions', function() {
       assert.deepEqual(jt.tokenize('a+1').tokens, [{value: 'a', identifier: true}, {value: '+', operator: true}, {value: 1, type: 'literal'}]);
       assert.deepEqual(jt.tokenize('a-1').tokens, [{value: 'a', identifier: true}, {value: '-', operator: true}, {value: 1, type: 'literal'}]);
       assert.deepEqual(jt.tokenize('a + 1').tokens, [{value: 'a', identifier: true}, {value: '+', operator: true}, {value: 1, type: 'literal'}]);
@@ -34,7 +34,7 @@ describe('jelTokenizer', ()=>{
     });
 
     
-    it('should provide peek(), last() and next()', ()=>{
+    it('should provide peek(), last() and next()', function() {
       const t = jt.tokenize('1 2 3 4 5');
       assert.equal(t.peek().value, 1);
       assert.equal(t.peek().value, 1);
@@ -52,7 +52,7 @@ describe('jelTokenizer', ()=>{
       assert.equal(t.next(), undefined);
     });
     
-    it('should provide copy()', ()=>{
+    it('should provide copy()', function() {
       const t = jt.tokenize('1 2 3 4 5');
       assert.equal(t.next().value, 1);
       const t2 = t.copy();
